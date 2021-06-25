7cad773ffd7a670d12cc9c068a542c3fd342cc10
Ishan  Garg <ishan@Ishans-MacBook-Air.local>
2021-06-25 21:46:48 +0530

    created index files

---
+++ index.js
@@ -0,0 +1,6 @@
+const searchForm = document.querySelector('#search-form');
+const searchInput = document.querySelector('#search-input');
+
+searchForm.addEventListener('submit', (e) =>{
+    e.preventDefault();
+});
